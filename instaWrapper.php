afaa086a04bcd176da571a150057cbfa3a6ce21d
Egor Zaretskiy <gorcer@gmail.com>
2018-05-02 10:38:09 +1000

    add proxy & curl:

--- instaWrapper.php
+++ instaWrapper.php
@@ -8,12 +8,27 @@
 
 class instaWrapper {
 
-    static function getFeedByUrl($url) {
+    static function getFeedByUrl($url, $proxy=false) {
 
     if ($url == "") return;
 
-        $source = file_get_contents($url);
-        if ($source == false) {
+     //   $source = file_get_contents($url);
+
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL,$url);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+	    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36');
+
+	    if ($proxy !== false) {
+		    curl_setopt($ch, CURLOPT_PROXYTYPE, 7);
+		    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+	    }
+
+	    $source=curl_exec($ch);
+	    curl_close($ch);
+
+
+        if ($source == false || $source == ' ') {
             echo "Connection problem";
             die();
         }
